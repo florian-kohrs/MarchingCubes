02e01784ae63bb5ee27df4bf65186231b59912e5
Florian <florian.kohrs@web.de>
2021-12-13 23:46:10 +0100

    Improve Asset Creator readabilty

--- Assets/Scripts/Editor/AssetHelper/AssetCreator.cs
+++ Assets/Scripts/Editor/AssetHelper/AssetCreator.cs
@@ -8,16 +8,37 @@ using UnityEngine;
 public class AssetCreator
 {
 
+    public const string ASSET_PATH = "Assets";
+    public const string ASSET_FILE_EXTENSION = ".asset";
+    public const char DIRECTORY_SEPERATOR = '/';
+
     /// <summary>
     /// creates an asset of type t at given path
     /// </summary>
     /// <typeparam name="T">type of asset</typeparam>
     /// <param name="path">relative path to safe assets</param>
     /// <param name="selectAsset">will focus the asset in inspector</param>
-    public static void CreateAsset<T>(string path, bool selectAsset = true)
+    public static void CreateAsset<T>(string path, bool selectAsset = true) where T : ScriptableObject
     {
-        string currentPath = "Assets";
-        foreach (string s in path.Split('/'))
+        string localPath = CreatePath(path);
+        ScriptableObject asset = CreateAsset<T>(localPath);
+
+        if (selectAsset)
+        {
+            Selection.activeObject = asset;
+            EditorGUIUtility.PingObject(asset);
+        }
+    }
+
+    /// <summary>
+    /// creates the directories to given path
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns>returns the relative path starting at assets</returns>
+    private static string CreatePath(string path)
+    {
+        string currentPath = ASSET_PATH;
+        foreach (string s in path.Split(DIRECTORY_SEPERATOR))
         {
             string nextPath = Path.Combine(currentPath, s);
             if (!AssetDatabase.IsValidFolder(nextPath))
@@ -26,16 +47,17 @@ public class AssetCreator
             }
             currentPath = nextPath;
         }
+        return currentPath;
+    }
 
-        ScriptableObject asset = ScriptableObject.CreateInstance(typeof(T));
-        AssetDatabase.CreateAsset(asset, Path.Combine(currentPath, System.Guid.NewGuid().ToString()) + ".asset");
+
+    private static ScriptableObject CreateAsset<T>(string path) where T : ScriptableObject
+    {
+        ScriptableObject asset = ScriptableObject.CreateInstance<T>();
+        AssetDatabase.CreateAsset(asset, Path.Combine(path, System.Guid.NewGuid().ToString()) + ASSET_FILE_EXTENSION);
         AssetDatabase.SaveAssets();
         AssetDatabase.Refresh();
         EditorUtility.FocusProjectWindow();
-        if (selectAsset)
-        {
-            Selection.activeObject = asset;
-            EditorGUIUtility.PingObject(asset);
-        }
+        return asset;
     }
 }
